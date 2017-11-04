948e819e5fb8c6acd83537c72dba14395444ee27
Nathaniel <gorton.nj@gmail.com>
2017-11-04 20:21:15 +0000

    configure environment variables

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
-// mongoose.connect("mongodb://njgorton:elanor12@ds147265.mlab.com:47265/yelp_camp", {useMongoClient: true});
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12_Deployed";
+mongoose.connect(url, {useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
